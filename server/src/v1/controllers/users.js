ac2267e1e0278895daedeaa30a5d707ed19e0781
oneEyedSunday <idiakosesunday@gmail.com>
2018-11-07 12:48:42 +0100

    feature: Implemented GET /api/v1/users/<userId>/parcels

--- server/src/v1/controllers/users.js
+++ server/src/v1/controllers/users.js
@@ -1,3 +1,5 @@
+import parcelsController from './parcels';
+
 const users = [
   {
     id: 1,
@@ -11,9 +13,24 @@ const UsersController = {
     return users;
   },
 
+  getUser(userId) {
+    return users.filter(items => items.id === userId)[0];
+  },
+
+  parcelsForUser(userId) {
+    const user = UsersController.getUser(userId);
+    return user.parcels;
+  },
+
   index(req, res) {
     const allUsers = UsersController.findAll();
     return res.json(allUsers);
+  },
+
+  parcels(req, res) {
+    const userParcels = UsersController.parcelsForUser(parseInt(req.params.id, 10));
+    const populatedUserParcels = parcelsController.retrieveParcels(userParcels);
+    return res.json(populatedUserParcels);
   },
 };
 
--- server/src/v1/routes/users.js
+++ server/src/v1/routes/users.js
@@ -3,4 +3,5 @@ import usersController from '../controllers/users';
 
 const router = Router();
 router.get('/', usersController.index);
+router.get('/:id/parcels', usersController.parcels);
 export default router;
