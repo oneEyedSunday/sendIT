9a2e4c91f5eb24972aa94c7f0b5802282ade8aec
oneEyedSunday <idiakosesunday@gmail.com>
2018-11-16 12:15:22 +0100

    [Chore #162009238] Provision DB for CI

--- server/src/v1/helpers/db/index.js
+++ server/src/v1/helpers/db/index.js
@@ -15,7 +15,9 @@ const createPool = () => {
   if (process.env.DATABASE_URL) poolObj = { connectionString: process.env.DATABASE_URL };
   // console.log(process.env.NODE_ENV);
 
-  if (process.env.NODE_ENV === 'test') poolObj.database = process.env.DB_NAME_TEST;
+  if (process.env.NODE_ENV === 'test') poolObj.database = process.env.DB_HEROKU;
+  // if (process.env.NODE_ENV === 'heroku') poolObj.database = process.env.DB_HEROKU;
+
   // console.log(poolObj);
   return new Pool(poolObj);
 };
